f849839325160ed85e4de9d831ec6e3f356d76a9
luismrosero <luismrosero1992@gmail.com>
2021-10-13 10:18:16 -0500

    change from luis

--- src/leiva/HelloWorld.java
+++ src/leiva/HelloWorld.java
@@ -2,7 +2,11 @@ package leiva;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         String name = "Joaquin";
+        String companion = "Luis";
         System.out.println("Hello my name is " + name);
+        System.out.println("Hello my name is " + companion);
+
     }
 }
