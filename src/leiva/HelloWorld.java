bba9e75f8a237132ec57275844f1d325a287b9bf
joakinleiva <joako.leiva9@gmail.com>
2021-10-13 11:55:32 -0300

    DOne

---
+++ src/leiva/HelloWorld.java
@@ -0,0 +1,8 @@
+package leiva;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "Joaquin";
+        System.out.println("Hello my name is " + name);
+    }
+}
